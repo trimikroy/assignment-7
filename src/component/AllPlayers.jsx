8c6626e224b9bf336ac56178572da5ab8f0b9c43
trimikpro <vupotiroy112@gmail.com>
2025-01-04 06:41:17 +0600

    fake data fetch to map pleyer and available data show

--- src/component/AllPlayers.jsx
+++ src/component/AllPlayers.jsx
@@ -1,31 +1,31 @@
-// import Available from '../component/SinglePlayer'
-// import { useEffect, useState } from "react";
+import Available from '../component/SinglePlayer'
+import { useEffect, useState } from "react";
 
 
-// const AllPlayers = ({handleselectedPlayers}) => {
-//   const [players, setPlayers] = useState([]);
+const AllPlayers = ({handleselectedPlayers}) => {
+  const [players, setPlayers] = useState([]);
 
-//   useEffect(() => {
-//     fetch('./Players.json')
-//       .then(res => res.json())
-//     .then(data=> setPlayers(data))
-//   }, [])
+  useEffect(() => {
+    fetch('./Players.json')
+      .then(res => res.json())
+    .then(data=> setPlayers(data))
+  }, [])
   
   
-//   return (
-//     <div>
+  return (
+    <div>
       
-//     <div className='grid lg:grid-cols-3 gap-8'>
-//       {
-//           players.map((player =><Available handleselectedPlayers={handleselectedPlayers} key={player.player_Id} player={player}></Available>))
+    <div className='grid lg:grid-cols-3 gap-8'>
+      {
+          players.map((player =><Available handleselectedPlayers={handleselectedPlayers} key={player.player_Id} player={player}></Available>))
 
-//         }
-//         </div>
-//         </div>
+        }
+        </div>
+        </div>
 
     
     
-//   );
-// };
+  );
+};
 
-// export default AllPlayers;
+export default AllPlayers;
