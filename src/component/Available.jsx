47e91a2722cfa02e1f2e4d56a04da8918463bbb8
trimikpro <vupotiroy112@gmail.com>
2025-01-04 06:42:31 +0600

    avaiable pleyer and selected player

--- src/component/Available.jsx
+++ src/component/Available.jsx
@@ -1,66 +1,66 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-// const Available = ({handleselectedPlayers}) => {
-// const [players, setPlayers] = useState([]);
+const Available = ({handleselectedPlayers}) => {
+const [players, setPlayers] = useState([]);
 
-//   useEffect(() => {
-//     fetch('./Players.json')
-//       .then(res => res.json())
-//     .then(data=> setPlayers(data))
-//   }, [])
+  useEffect(() => {
+    fetch('./Players.json')
+      .then(res => res.json())
+    .then(data=> setPlayers(data))
+  }, [])
 
 
 
-//   return (
-//     <div className="grid lg:grid-cols-3 gap-8 mt-7">
-//       {players.map(player => (
-//       <div key={player.player_Id} className="">
-//       <div className="card bg-base-100 shadow-xl flex  {player_Id} ">
-//   <figure>
-//     <img className="h-[250px] rounded-lg w-full"
-//       src={player.image}
-//       alt="Players img" />
-//   </figure>
-//    <div className="card-body">
-//  <div className="flex items-center gap-5">
-//      <div className="avatar">
-//   <div className="ring-primary ring-offset-base-100 w-8 rounded-full ring ring-offset-2">
-//     <img src={player.image} />
-//   </div>
-// </div>         
-//   <div>
-//   <h2 className="card-title font-bold">{player.name}</h2>         
-//    </div>
-//     </div>        
-//             {/*country flag  */}
-//             <div className="flex justify-between mt-3 mb-3">
-//               <div className="flex items-center gap-4 text-gray-400">
-//                 <i class="fa-solid fa-flag"></i>
-//                 <h3> {player.country}</h3>
-//               </div>
-//                <div className="">
-//                 <button className=" bg-gray-300 py-2 px-3 rounded-lg font-semibold">{player.role}</button>
-//            </div>
-//             </div>
-//             <hr />
-//             <h2 className="font-semibold">Rating</h2>
-//     <div className="flex justify-between mt-3">
-//               <div><h3 className="font-bold">{ player.batting_type}</h3></div>
-//               <div><h3 className="font-bold">{ player.bowling_type}</h3></div>
-//     </div>
-//     <div className="flex justify-between mt-3">
-//               <div><h3 className="font-bold">Price: { player.bidding_price}</h3></div>
-//               <div>  <button onClick={() => handleselectedPlayers(player)} className=" bg-[#e7fe29] py-2 px-3 rounded-lg font-semibold hover:bg-yellow-200">Choose Player</button></div>
-//     </div>
+  return (
+    <div className="grid lg:grid-cols-3 gap-8 mt-7">
+      {players.map(player => (
+      <div key={player.player_Id} className="">
+      <div className="card bg-base-100 shadow-xl flex  {player_Id} ">
+  <figure>
+    <img className="h-[250px] rounded-lg w-full"
+      src={player.image}
+      alt="Players img" />
+  </figure>
+   <div className="card-body">
+ <div className="flex items-center gap-5">
+     <div className="avatar">
+  <div className="ring-primary ring-offset-base-100 w-8 rounded-full ring ring-offset-2">
+    <img src={player.image} />
+  </div>
+</div>         
+  <div>
+  <h2 className="card-title font-bold">{player.name}</h2>         
+   </div>
+    </div>        
+            {/*country flag  */}
+            <div className="flex justify-between mt-3 mb-3">
+              <div className="flex items-center gap-4 text-gray-400">
+                <i class="fa-solid fa-flag"></i>
+                <h3> {player.country}</h3>
+              </div>
+               <div className="">
+                <button className=" bg-gray-300 py-2 px-3 rounded-lg font-semibold">{player.role}</button>
+           </div>
+            </div>
+            <hr />
+            <h2 className="font-semibold">Rating</h2>
+    <div className="flex justify-between mt-3">
+              <div><h3 className="font-bold">{ player.batting_type}</h3></div>
+              <div><h3 className="font-bold">{ player.bowling_type}</h3></div>
+    </div>
+    <div className="flex justify-between mt-3">
+              <div><h3 className="font-bold">Price: { player.bidding_price}</h3></div>
+              <div>  <button onClick={() => handleselectedPlayers(player)} className=" bg-[#e7fe29] py-2 px-3 rounded-lg font-semibold hover:bg-yellow-200">Choose Player</button></div>
+    </div>
    
     
-//   </div>
-// </div>
-//     </div>
+  </div>
+</div>
+    </div>
     
-//    ))}
-//     </div>
-//   );
-// };
+   ))}
+    </div>
+  );
+};
 
-// export default Available;
+export default Available;
--- src/component/Seleted.jsx
+++ src/component/Seleted.jsx
@@ -1,36 +1,36 @@
 
-// const Seleted = ({ selectedPlayer, handleRemoveBtn }) => {
+const Seleted = ({ selectedPlayer, handleRemoveBtn }) => {
   
-//   return (
-//     <div className="mt-7">
-//       {
-//       selectedPlayer.map((player) => ( 
-//         <div key={player.player_Id} className="flex justify-between border-2 border-gray-400 rounded-lg p-2 m-3 items-center">
+  return (
+    <div className="mt-7">
+      {
+      selectedPlayer.map((player) => ( 
+        <div key={player.player_Id} className="flex justify-between border-2 border-gray-400 rounded-lg p-2 m-3 items-center">
 
-//       <div><div className="">
-//            <div className="flex items-center gap-5">
-//      <div className="avatar">
-//   <div className="ring-primary ring-offset-base-100 w-8 rounded-full ring ring-offset-2 ml-6">
-//     <img className="gap-8" src={player.image} />
-//   </div>
-// </div>         
-//     <div>
-//     <div>
-//   <h2 className="card-title font-bold">{player.name}</h2>         
-// </div>
-//  <div><h5>{player. batting_type}</h5></div>
-//  </div>
-//     </div>
-//           </div></div>
-//           <div>
-//             <button onClick={() => handleRemoveBtn(player.player_Id)}>
-//               <i className="fa-solid fa-trash text-2xl mr-6"></i>
-//             </button>
-//      </div>
-//         </div>))
-//     }
-//     </div>
-//   );
-// };
+      <div><div className="">
+           <div className="flex items-center gap-5">
+     <div className="avatar">
+  <div className="ring-primary ring-offset-base-100 w-8 rounded-full ring ring-offset-2 ml-6">
+    <img className="gap-8" src={player.image} />
+  </div>
+</div>         
+    <div>
+    <div>
+  <h2 className="card-title font-bold">{player.name}</h2>         
+</div>
+ <div><h5>{player. batting_type}</h5></div>
+ </div>
+    </div>
+          </div></div>
+          <div>
+            <button onClick={() => handleRemoveBtn(player.player_Id)}>
+              <i className="fa-solid fa-trash text-2xl mr-6"></i>
+            </button>
+     </div>
+        </div>))
+    }
+    </div>
+  );
+};
 
-// export default Seleted;
+export default Seleted;
