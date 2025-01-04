07c68acccac8b9c97d29a52f645b4fba2f531fcb
trimikpro <vupotiroy112@gmail.com>
2025-01-04 06:38:06 +0600

    fake json file create and disply show card

--- src/component/PlayersCard.jsx
+++ src/component/PlayersCard.jsx
@@ -1,24 +1,24 @@
-// import Available from './Available'
-// import Seleted from './Seleted'
+import Available from './Available'
+import Seleted from './Seleted'
 
-// const PlayersCard = ({ handleIsActiveStatus, isActive, handleselectedPlayers, selectedPlayer, handleRemoveBtn }) => {
+const PlayersCard = ({ handleIsActiveStatus, isActive, handleselectedPlayers, selectedPlayer, handleRemoveBtn }) => {
 
-//   return (
-//     <div className=''> 
+  return (
+    <div className=''> 
       
-//       <div className="flex gap-2 justify-end mt-8 ">
+      <div className="flex gap-2 justify-end mt-8 ">
        
-//         <div className="border border-gray-500 flex justify-center rounded-lg font-bold">
-//           <button onClick={() => handleIsActiveStatus("cart")} className={`${isActive.cart ? "bg-[#e7fe29] font-bold rounded-md text-black py-3 px-3" : ""}`}>Available</button>
-//         </div>
-//         <div className="border border-gray-500 flex justify-center rounded-lg font-bold" >
-//           <button onClick={() => handleIsActiveStatus("selected")} className={`${isActive.cart ? "" : "bg-[#e7fe29] font-bold rounded-md text-black py-3 px-3"}`}>selected ({selectedPlayer.length})</button>
-//         </div>
-//       </div>
-//       {isActive.cart ? <Available handleselectedPlayers={handleselectedPlayers}></Available> :
-//         <Seleted selectedPlayer={selectedPlayer} handleRemoveBtn={handleRemoveBtn}></Seleted>}
-//     </div>
-//   );
-// };
+        <div className="border border-gray-500 flex justify-center rounded-lg font-bold">
+          <button onClick={() => handleIsActiveStatus("cart")} className={`${isActive.cart ? "bg-[#e7fe29] font-bold rounded-md text-black py-3 px-3" : ""}`}>Available</button>
+        </div>
+        <div className="border border-gray-500 flex justify-center rounded-lg font-bold" >
+          <button onClick={() => handleIsActiveStatus("selected")} className={`${isActive.cart ? "" : "bg-[#e7fe29] font-bold rounded-md text-black py-3 px-3"}`}>selected ({selectedPlayer.length})</button>
+        </div>
+      </div>
+      {isActive.cart ? <Available handleselectedPlayers={handleselectedPlayers}></Available> :
+        <Seleted selectedPlayer={selectedPlayer} handleRemoveBtn={handleRemoveBtn}></Seleted>}
+    </div>
+  );
+};
 
-// export default PlayersCard;
+export default PlayersCard;
